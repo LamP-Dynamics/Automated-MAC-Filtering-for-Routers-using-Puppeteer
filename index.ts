57b3094a07cd8cee692ba703184381cdb53634ea
SalamPS <salamp@salamp.id>
2024-12-11 00:20:12 +0700

    refactor(index): Minimized

--- index.ts
+++ index.ts
@@ -1,52 +1,63 @@
 import puppeteer from "puppeteer";
-import { sendLog } from "./src/utils";
+import { Data, sendLog } from "./src/utils";
 import * as admin from "./src"
 
 (async () => {
   // Dummy datas
-  const fetched = [
-    { mac: 'c8:5b:76:17:a3:5b', name: 'mac_5b' },
-    { mac: '98:b0:8b:2a:4c:90', name: 'mac_90' },
-    { mac: '10:63:c8:8c:fa:cd', name: 'mac_cd' },
-    { mac: '04:f9:93:f2:46:5d', name: 'mac_5d' },
-    { mac: 'c0:18:03:2b:42:30', name: 'mac_30' },
-    { mac: 'b4:31:61:95:4f:cd', name: 'mac_cd' },
-    { mac: '28:8c:ec:78:f0:a5', name: 'mac_a5' },
-    { mac: '98:b0:8b:ff:7b:22', name: 'mac_22' },
-    { mac: '10:e7:c6:db:25:d9', name: 'mac_d9' },
-    { mac: 'd0:9c:7a:04:38:a4', name: 'mac_a4' },
-    { mac: '14:13:33:8a:46:23', name: 'mac_23' },
-    { mac: '8c:aa:ce:20:a3:b6', name: 'mac_b6' },
-    { mac: '68:bf:c4:dc:62:f6', name: 'mac_f6' },
-    { mac: '60:3e:5f:21:33:99', name: 'mac_99' },
-  ]
+  const fetched:Data[] = []
 
   let trial = 0;
   while (trial >= 0) {
-    try {
-      const DIR605_URL  = process.env.DIR605_URL
-      const WR840N_URL   = process.env.WR840N_URL
+    const DIR605_URL = process.env.DIR605_URL
+    const WR840N_URL = process.env.WR840N_URL
+    const ADMIN_URL  = process.env.ADMIN_URL
+    if ((DIR605_URL || WR840N_URL) && ADMIN_URL) {
+      sendLog("Fetching data from API...")
+      const today = new Date()
+      await fetch(ADMIN_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+          bulan: today.getDate() > 20 ? today.getMonth() + 1 : today.getMonth(),
+          tahun: today.getFullYear()
+        })
+      })
+      .then(async res => {
+        if (!res.ok) throw new Error('Failed to fetch data from API')
+        const data:Data[] = await res.json()
+        data.forEach((item:Data) => {
+          fetched.push(item)
+        })
+      })
+      .catch(err => {
+        sendLog('Failed to fetch data from API')
+        console.log(err)
+        trial--
+      })
+      if (trial < 0) return sendLog('Exiting app...')
 
-      if (DIR605_URL || WR840N_URL) {
-        const browser = await puppeteer.launch({ 
-          headless: false, 
-          executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' 
-        });
+      sendLog("Opening browser...")
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      })
 
-        if (DIR605_URL) await admin.DIR605l(DIR605_URL, browser, fetched)
-
-        //// Currently in development
-        // if (WR840N_URL) await admin.WR840N(WR840N_URL, browser, fetched)
-
-        await browser.close();
+      try {
+        // if (DIR605_URL) await admin.DIR605l(DIR605_URL, browser, fetched)
+        if (WR840N_URL) await admin.WR840N(WR840N_URL, browser, fetched)
         sendLog("Exiting app...")
         trial--;
       }
-      else throw new Error('No {ADMIN}_URL found in .env file')
+      catch (err) {
+        sendLog(err + `\nFailed. Retrying for ${trial} time...`)
+        trial++
+      }
+      if (trial < 0) {
+        return await browser.close()
+      }
     }
-    catch (err) {
-      sendLog(err + `\nFailed. Retrying for ${trial} time...`)
-      trial++
-    }
+    else sendLog('Env variables is not fully required. See our the documentation on github for more information.')
   }
 })();
