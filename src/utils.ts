6c58ab01844ec18c1a830d7998a388ca4606a4f4
SalamPS <salamp@salamp.id>
2024-12-11 02:52:47 +0700

    feat(interface, click, puppet): Referring to new frame system

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { ElementHandle, Page } from "puppeteer";
+import { ElementHandle, Frame, Page } from "puppeteer";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,17 +8,19 @@ export const sendLog = (message:string | unknown) => {
   console.log('=========================')
   console.log(message)
 }
-export const puppet = async (page: Page, callback: () => Promise<void>, message:string):Promise<void> => {
+export const puppet = async (page: Page, callback: () => Promise<void>, message:string, selector?:string):Promise<void> => {
   console.log(message)
-  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  if (!selector) await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  else await page.$eval(selector, () => {console.log('Evaluated')});
   await callback();
 }
 export interface Data {
   mac: string;
   name: string;
   index?: number;
+  enabled?: boolean;
 }
-export const click = async (page: Page | ElementHandle, selector: string):Promise<void> => {
+export const click = async (page: Page | ElementHandle | Frame, selector: string):Promise<void> => {
   const element = await page.$(selector);
   if (element) {
     await element.click();
